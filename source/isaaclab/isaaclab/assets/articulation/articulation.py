d9f93aa5da50e22b46f9df34e6ace3f3bd5b3ba2
Antoine Richard <antoiner@nvidia.com>
2025-10-16 16:27:32 +0200

    pre-commits

--- source/isaaclab/isaaclab/assets/articulation/articulation.py
+++ source/isaaclab/isaaclab/assets/articulation/articulation.py
@@ -1004,7 +1004,9 @@ class Articulation(AssetBase):
         if body_ids is None:
             body_ids = self._ALL_BODY_INDICES_WP
         elif isinstance(body_ids, slice):
-            body_ids = wp.from_torch(torch.arange(self.num_bodies, dtype=torch.int32, device=self.device)[body_ids], dtype=wp.int32)
+            body_ids = wp.from_torch(
+                torch.arange(self.num_bodies, dtype=torch.int32, device=self.device)[body_ids], dtype=wp.int32
+            )
         elif not isinstance(body_ids, torch.Tensor):
             body_ids = wp.array(body_ids, dtype=wp.int32, device=self.device)
         else:
@@ -1018,7 +1020,6 @@ class Articulation(AssetBase):
             positions=wp.from_torch(positions, dtype=wp.vec3f) if positions is not None else None,
             is_global=is_global,
         )
-
 
     def add_permanent_external_wrench(
         self,
@@ -1068,7 +1069,9 @@ class Articulation(AssetBase):
         if body_ids is None:
             body_ids = self._ALL_BODY_INDICES_WP
         elif isinstance(body_ids, slice):
-            body_ids = wp.from_torch(torch.arange(self.num_bodies, dtype=torch.int32, device=self.device)[body_ids], dtype=wp.int32)
+            body_ids = wp.from_torch(
+                torch.arange(self.num_bodies, dtype=torch.int32, device=self.device)[body_ids], dtype=wp.int32
+            )
         elif not isinstance(body_ids, torch.Tensor):
             body_ids = wp.array(body_ids, dtype=wp.int32, device=self.device)
         else:
@@ -1148,7 +1151,9 @@ class Articulation(AssetBase):
         if body_ids is None:
             body_ids = self._ALL_BODY_INDICES_WP
         elif isinstance(body_ids, slice):
-            body_ids = wp.from_torch(torch.arange(self.num_bodies, dtype=torch.int32, device=self.device)[body_ids], dtype=wp.int32)
+            body_ids = wp.from_torch(
+                torch.arange(self.num_bodies, dtype=torch.int32, device=self.device)[body_ids], dtype=wp.int32
+            )
         elif not isinstance(body_ids, torch.Tensor):
             body_ids = wp.array(body_ids, dtype=wp.int32, device=self.device)
         else:
@@ -2319,4 +2324,6 @@ class Articulation(AssetBase):
             "The function 'set_external_force_and_torque' will be deprecated in a future release. Please"
             " use 'set_permanent_external_wrench' instead."
         )
-        self.set_permanent_external_wrench(forces=forces, torques=torques, positions=positions, body_ids=body_ids, env_ids=env_ids, is_global=is_global)
+        self.set_permanent_external_wrench(
+            forces=forces, torques=torques, positions=positions, body_ids=body_ids, env_ids=env_ids, is_global=is_global
+        )
--- source/isaaclab/isaaclab/assets/rigid_object_collection/rigid_object_collection.py
+++ source/isaaclab/isaaclab/assets/rigid_object_collection/rigid_object_collection.py
@@ -468,6 +468,7 @@ class RigidObjectCollection(AssetBase):
     """
     Operations - Setters.
     """
+
     def set_permanent_external_wrench(
         self,
         forces: torch.Tensor | None = None,
@@ -522,7 +523,9 @@ class RigidObjectCollection(AssetBase):
         if object_ids is None:
             object_ids = self._ALL_OBJ_INDICES_WP
         elif isinstance(object_ids, slice):
-            object_ids = wp.from_torch(torch.arange(self.num_objects, dtype=torch.int32, device=self.device)[object_ids], dtype=wp.int32)
+            object_ids = wp.from_torch(
+                torch.arange(self.num_objects, dtype=torch.int32, device=self.device)[object_ids], dtype=wp.int32
+            )
         elif not isinstance(object_ids, torch.Tensor):
             object_ids = wp.array(object_ids, dtype=wp.int32, device=self.device)
         else:
@@ -586,7 +589,9 @@ class RigidObjectCollection(AssetBase):
         if object_ids is None:
             object_ids = self._ALL_OBJ_INDICES_WP
         elif isinstance(object_ids, slice):
-            object_ids = wp.from_torch(torch.arange(self.num_objects, dtype=torch.int32, device=self.device)[object_ids], dtype=wp.int32)
+            object_ids = wp.from_torch(
+                torch.arange(self.num_objects, dtype=torch.int32, device=self.device)[object_ids], dtype=wp.int32
+            )
         elif not isinstance(object_ids, torch.Tensor):
             object_ids = wp.array(object_ids, dtype=wp.int32, device=self.device)
         else:
@@ -666,7 +671,9 @@ class RigidObjectCollection(AssetBase):
         if object_ids is None:
             object_ids = self._ALL_OBJ_INDICES_WP
         elif isinstance(object_ids, slice):
-            object_ids = wp.from_torch(torch.arange(self.num_objects, dtype=torch.int32, device=self.device)[object_ids], dtype=wp.int32)
+            object_ids = wp.from_torch(
+                torch.arange(self.num_objects, dtype=torch.int32, device=self.device)[object_ids], dtype=wp.int32
+            )
         elif not isinstance(object_ids, torch.Tensor):
             object_ids = wp.array(object_ids, dtype=wp.int32, device=self.device)
         else:
@@ -939,4 +946,11 @@ class RigidObjectCollection(AssetBase):
             "The function 'set_external_force_and_torque' will be deprecated in a future release. Please"
             " use 'set_permanent_external_wrench' instead."
         )
-        self.set_permanent_external_wrench(forces=forces, torques=torques, positions=positions, object_ids=object_ids, env_ids=env_ids, is_global=is_global)
+        self.set_permanent_external_wrench(
+            forces=forces,
+            torques=torques,
+            positions=positions,
+            object_ids=object_ids,
+            env_ids=env_ids,
+            is_global=is_global,
+        )
